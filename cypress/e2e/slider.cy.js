397be7ea6b1f75553d05c292b2445c8453c1319c
Filip Piskorski <fpfpfp2002@gmail.com>
2025-03-20 11:43:18 +0100

    heatmaps and tests

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,31 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if the user can swipe slides both sides', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-fully-visible').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-prev').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-fully-visible').should('contain', 'Rome');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if the title and description is visible and appropriate for every slide', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-fully-visible').should('contain', 'Rome');
+    cy.get('.swiper-slide-fully-visible').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-fully-visible').should('contain', 'London');
+    cy.get('.swiper-slide-fully-visible').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-fully-visible').should('contain', 'Paris');
+    cy.get('.swiper-slide-fully-visible').should('contain', 'France');
+  });
+});
